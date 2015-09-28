5432f021e723f272250e778ca6478867a105f60d
Grzegorz Grzybek <gr.grzybek@gmail.com>
2015-09-28 11:22:21 +0200

    [git-patching] Adding test for ZIP patch file without descriptor

--- patch/patch-management/src/test/java/io/fabric8/patch/management/PatchManagementTest.java
+++ patch/patch-management/src/test/java/io/fabric8/patch/management/PatchManagementTest.java
@@ -18,16 +18,11 @@ package io.fabric8.patch.management;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.nio.file.Files;
-import java.nio.file.attribute.FileAttributeView;
 import java.nio.file.attribute.PosixFilePermission;
-import java.nio.file.attribute.PosixFilePermissions;
 import java.util.List;
 import java.util.Properties;
 import java.util.Set;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipOutputStream;
 
 import io.fabric8.patch.management.impl.GitPatchManagementServiceImpl;
 import io.fabric8.patch.management.impl.Utils;
@@ -46,6 +41,8 @@ import org.osgi.framework.BundleContext;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -89,10 +86,11 @@ public class PatchManagementTest {
 
         // prepare some ZIP patches
         preparePatchZip("src/test/resources/content/patch1", "target/karaf/patches/source/patch-1.zip");
+        preparePatchZip("src/test/resources/content/patch3", "target/karaf/patches/source/patch-3.zip");
     }
 
     @Test
-    public void fetchPatch1() throws IOException {
+    public void fetchPatch1FromZipFileWithDescriptor() throws IOException {
         List<PatchData> patches = pm.fetchPatches(new File("target/karaf/patches/source/patch-1.zip").toURI().toURL());
         assertThat(patches.size(), equalTo(1));
         PatchData data = patches.get(0);
@@ -105,6 +103,39 @@ public class PatchManagementTest {
         assertThat("Fetching a patch should extract maven artifacts immediately",
                 FileUtils.readFileToString(new File(karafHome, "system/io/fabric8/fabric-tranquility/1.2.3/fabric-tranquility-1.2.3.jar")),
                 equalTo("JAR\n"));
+    }
+
+    @Test
+    public void fetchPatch2AsPlainDescriptor() throws IOException {
+        List<PatchData> patches = pm.fetchPatches(new File("src/test/resources/descriptors/my-patch-2.patch").toURI().toURL());
+        assertThat(patches.size(), equalTo(1));
+        PatchData data = patches.get(0);
+        assertThat(data.getId(), equalTo("my-patch-2"));
+        assertThat(data.getBundles().size(), equalTo(1));
+        assertThat(data.getBundles().iterator().next(), equalTo("mvn:io.fabric8/fabric-tranquility/1.2.3"));
+        assertThat(data.getFiles().size(), equalTo(0));
+
+        assertFalse(new File(karafHome, "patches/my-patch-2").exists());
+        assertTrue(new File(karafHome, "patches/my-patch-2.patch").exists());
+        assertTrue(new File(karafHome, "patches/my-patch-2.patch").isFile());
+    }
+
+    @Test
+    public void fetchPatch3FromZipFileWithoutDescriptor() throws IOException {
+        List<PatchData> patches = pm.fetchPatches(new File("target/karaf/patches/source/patch-3.zip").toURI().toURL());
+        assertThat(patches.size(), equalTo(1));
+        PatchData data = patches.get(0);
+        assertTrue(data.isGenerated());
+        assertThat(data.getId(), equalTo("patch-3"));
+        assertThat(data.getBundles().size(), equalTo(1));
+        assertThat(data.getBundles().iterator().next(), equalTo("io/fabric8/fabric-colours/1.2.3/fabric-colours-1.2.3.jar"));
+        assertThat(data.getFiles().size(), equalTo(2));
+
+        assertThat(FileUtils.readFileToString(new File(karafHome, "patches/patch-3/bin/stop")), equalTo("#!/bin/bash\n\necho \"stopped\"\n"));
+        assertThat("Fetching a patch should extract maven artifacts immediately",
+                FileUtils.readFileToString(new File(karafHome, "system/io/fabric8/fabric-colours/1.2.3/fabric-colours-1.2.3.jar")),
+                equalTo("JAR\n"));
+        assertFalse("Maven artifacts should not be extracted to patch data directory", new File(karafHome, "patches/patch-3/system").exists());
     }
 
     private void preparePatchZip(String directoryToZip, String zipFile) throws IOException {
