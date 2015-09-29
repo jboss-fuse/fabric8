069a610cfc696fbdf70aba8af93d71626aac0de4
Paolo Antinori <pantinor@redhat.com>
2015-09-29 15:51:05 +0200

    ENTESB-3851 - Improve debug info for ssh provisioning

--- fabric/fabric-core/src/main/resources/io/fabric8/internal/download.sh
+++ fabric/fabric-core/src/main/resources/io/fabric8/internal/download.sh
@@ -1,6 +1,6 @@
 function download {
   echo "Downloading: $1";
-  ret=`curl --write-out %{http_code} --silent --output $2 $1`;
+  ret=`curl -C - --retry 10 --write-out %{http_code} --silent --output $2 $1`;
   if [ "${ret}" -ne 200 ]; then
     echo "Download failed with code: ${ret}";
     rm $2;
--- fabric/fabric-core/src/main/resources/io/fabric8/internal/exit_if_not_exists.sh
+++ fabric/fabric-core/src/main/resources/io/fabric8/internal/exit_if_not_exists.sh
@@ -2,5 +2,12 @@ function exit_if_not_exists() {
  if [ ! -f $1 ]; then
           echo "Command Failed:Could not find file $1";
           exit -1;
-  fi
+ fi
+ local zipFile="$1"
+ local size="$(du $zipFile |  awk '{ print $1}')"
+ if [ $size -lt 100 ]; then
+          echo "Command Failed: Zip archive is empty. Check $1";
+          exit -1;
+ fi
+
 }
