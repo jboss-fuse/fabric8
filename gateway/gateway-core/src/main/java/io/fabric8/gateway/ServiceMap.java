a7afa60ccf9089f136aa988683b5feffcbcf2cd5
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2016-12-16 11:25:49 +0900

    [PATCH-1620] 6.3 HF #5-ENTESB-6254  Fabric MQ gateway is left in a "no endpoints available" condition after failover, on distributed AWS system
    (cherry picked from commit 4d20061e3b5f80c49441dd1b80f020364d4c6f88)

--- gateway/gateway-core/src/main/java/io/fabric8/gateway/ServiceMap.java
+++ gateway/gateway-core/src/main/java/io/fabric8/gateway/ServiceMap.java
@@ -19,27 +19,27 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 
 /**
  * Maintains a mapping of services which is then use by the proxy to update in process
  * proxy handlers, or used to create new proxy handers
  */
 public class ServiceMap {
-    private ConcurrentHashMap<String, Map<String, ServiceDetails>> map = new ConcurrentHashMap<String, Map<String, ServiceDetails>>();
+    private final ConcurrentMap<String, PathMap> map = new ConcurrentHashMap<>();
 
     /**
      * Returns a list of all the current services for the given path
      */
     public List<ServiceDetails> getServices(String path) {
-        Map<String, ServiceDetails> pathMap = getPathMap(path);
-        return new ArrayList<ServiceDetails>(pathMap.values());
+        return getPathMap(path).getServices();
     }
 
     /**
      * Returns a list of all the current paths for the services
      */
     public List<String> getPaths() {
-        return new ArrayList<String>(map.keySet());
+        return new ArrayList<>(map.keySet());
     }
 
     /**
@@ -48,25 +48,71 @@ public class ServiceMap {
     public void serviceUpdated(String path, ServiceDetails service) {
         // ignore services with empty services
         if (!service.getServices().isEmpty()) {
-            getPathMap(path).put(service.getId(), service);
+            getPathMap(path).update(service);
         }
     }
 
     /**
-     * When a service is added or updated
+     * When a service is removed
      */
     public void serviceRemoved(String path, ServiceDetails service) {
-        getPathMap(path).remove(service.getId());
+        getPathMap(path).remove(service);
 
         // lets update any in progress proxy handlers using this service
     }
 
-    protected Map<String, ServiceDetails> getPathMap(String path) {
-        Map<String, ServiceDetails> initial = new ConcurrentHashMap<String, ServiceDetails>();
-        Map<String, ServiceDetails> answer = map.putIfAbsent(path, initial);
+    protected PathMap getPathMap(String path) {
+        PathMap initial = new PathMap(path);
+        PathMap answer = map.putIfAbsent(path, initial);
         if (answer == null) {
             answer = initial;
         }
         return answer;
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + ": " + map.toString();
+    }
+
+    protected static class PathMap {
+        private final String path;
+        /**
+         * Map: Service ID -> Container -> ServiceDetails
+         */
+        private final ConcurrentMap<String, Map<String, ServiceDetails>> pathMap = new ConcurrentHashMap<>();
+
+        public PathMap(String path) {
+            this.path = path;
+        }
+
+        public List<ServiceDetails> getServices() {
+            List<ServiceDetails> answer = new ArrayList<>();
+            for (Map<String, ServiceDetails> containerMap : pathMap.values()) {
+                answer.addAll(containerMap.values());
+            }
+            return answer;
+        }
+
+        public void update(ServiceDetails service) {
+            Map<String, ServiceDetails> initial = new ConcurrentHashMap<>();
+            Map<String, ServiceDetails> containerMap = pathMap.putIfAbsent(service.getId(), initial);
+            if (containerMap == null) {
+                containerMap = initial;
+            }
+            containerMap.put(service.getContainer(), service);
+        }
+
+        public void remove(ServiceDetails service) {
+            Map<String, ServiceDetails> containerMap = pathMap.get(service.getId());
+            if (containerMap != null) {
+                containerMap.remove(service.getContainer());
+            }
+        }
+
+        @Override
+        public String toString() {
+            return getClass().getSimpleName() + ": " + pathMap.toString();
+        }
     }
 }
