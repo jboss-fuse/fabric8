69bf08a59b968f94320ae597426783fa6a5e4a9b
Grzegorz Grzybek <gr.grzybek@gmail.com>
2017-02-07 20:37:18 +0100

    [PATCH-1711] Fix patching edge case, where working copy becomes not clean due to changed file permissions
    (cherry picked from commit d712a940f98b53a437f6e8995065ded25d29fd7c)

--- patch/patch-management/src/main/java/io/fabric8/patch/management/impl/GitPatchManagementServiceImpl.java
+++ patch/patch-management/src/main/java/io/fabric8/patch/management/impl/GitPatchManagementServiceImpl.java
@@ -2732,6 +2732,7 @@ public class GitPatchManagementServiceImpl implements PatchManagement, GitPatchM
         int count = 1;
 
         // we may have unadded changes - when file mode is changed
+        fork.reset().setMode(ResetCommand.ResetType.MIXED).call();
         fork.reset().setMode(ResetCommand.ResetType.HARD).call();
 
         while (it.hasPrevious()) {
@@ -2747,6 +2748,7 @@ public class GitPatchManagementServiceImpl implements PatchManagement, GitPatchM
             gitPatchRepository.prepareCommit(fork, userChange.getFullMessage()).call();
 
             // we may have unadded changes - when file mode is changed
+            fork.reset().setMode(ResetCommand.ResetType.MIXED).call();
             fork.reset().setMode(ResetCommand.ResetType.HARD).call();
         }
 
