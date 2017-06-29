d1ab50aa72691705011dee5174051034deb2e09b
Grzegorz Grzybek <gr.grzybek@gmail.com>
2017-06-29 10:44:48 +0200

    [ENTESB-7011] Improve logging related to DefaultPullPushPolicy#doPull

--- fabric/fabric-git/src/main/java/io/fabric8/git/PullPushPolicy.java
+++ fabric/fabric-git/src/main/java/io/fabric8/git/PullPushPolicy.java
@@ -18,36 +18,110 @@ package io.fabric8.git;
 import io.fabric8.api.GitContext;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
+import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.transport.CredentialsProvider;
 import org.eclipse.jgit.transport.PushResult;
 import org.eclipse.jgit.transport.RemoteRefUpdate;
 
 public interface PullPushPolicy {
 
+    /**
+     * Result of fetching state from central fabric git repository to local git repository
+     */
     interface PullPolicyResult {
-        
-        Set<String> localUpdateVersions();
-        
+
+        /**
+         * Names of local branches/versions that were updated (removed, created or their HEAD changed by fast-forward
+         * or rebase)
+         * @return
+         */
+        Map<String, BranchChange> localUpdateVersions();
+
+        /**
+         * Whether {@link PullPushPolicy#doPush(GitContext, CredentialsProvider)} is needed after pulling
+         * @return
+         */
         boolean remoteUpdateRequired();
-        
+
+        /**
+         * Names of all branches/versions found remotely (even if local branches are in sync), without locally
+         * deleted branches
+         * @return
+         */
         Set<String> getVersions();
-        
+
         Exception getLastException();
     }
 
+    /**
+     * Result of pushing state from local git repository to central fabric git repository
+     */
     interface PushPolicyResult {
-    
+
         List<PushResult> getPushResults();
-        
+
         List<RemoteRefUpdate> getAcceptedUpdates();
-        
+
         List<RemoteRefUpdate> getRejectedUpdates();
-        
+
         Exception getLastException();
     }
-    
+
+    /**
+     * Indication of a change performed to local branch - was it created? deleted? updated and how?
+     */
+    public static class BranchChange {
+
+        private String branch;
+        private Change change = Change.UP_TO_DATE;
+        private ObjectId ref1, ref2;
+        private String updateStatus;
+
+        public BranchChange(String branch) {
+            this.branch = branch;
+        }
+
+        public BranchChange created() {
+            change = Change.CREATED;
+            return this;
+        }
+
+        public BranchChange removed() {
+            change = Change.REMOVED;
+            return this;
+        }
+
+        public BranchChange updated(ObjectId previousRef, ObjectId newRef, String how) {
+            change = Change.UPDATED;
+            ref1 = previousRef;
+            ref2 = newRef;
+            updateStatus = how;
+            return this;
+        }
+
+        @Override
+        public String toString() {
+            if (change == Change.UPDATED) {
+                return String.format("%s%s", branch, String.format(change.description, updateStatus,
+                        ref1 == null ? "?" : ref1.getName(), ref2 == null ? "?" : ref2.getName()));
+            } else {
+                return String.format("%s%s", branch, change.description);
+            }
+        }
+
+        private static enum Change {
+            CREATED("(new branch)"), REMOVED("(deleted)"), UPDATED("(updated by %s: %s..%s)"), UP_TO_DATE("");
+            private String description;
+
+            Change(String description) {
+                this.description = description;
+            }
+        }
+    }
+
     /**
      * Pull the version/profile state from the remote repository
      */
