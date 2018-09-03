789c342e16787cc544fc5b83f98176b0d8a76b41
Grzegorz Grzybek <gr.grzybek@gmail.com>
2018-09-03 13:41:37 +0200

    [ENTESB-9340] Reapply FABRIC-718 and disable SASL in ZK client

--- fabric/fabric-zookeeper/src/main/java/org/apache/zookeeper/client/ZooKeeperSaslClient.java
+++ fabric/fabric-zookeeper/src/main/java/org/apache/zookeeper/client/ZooKeeperSaslClient.java
@@ -1,19 +1,17 @@
 /**
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
+ *  Copyright 2005-2016 Red Hat, Inc.
+ *
+ *  Red Hat licenses this file to you under the Apache License, version
+ *  2.0 (the "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ *  implied.  See the License for the specific language governing
+ *  permissions and limitations under the License.
  */
 
 package org.apache.zookeeper.client;
@@ -50,7 +48,9 @@ import org.slf4j.LoggerFactory;
 public class ZooKeeperSaslClient {
     public static final String LOGIN_CONTEXT_NAME_KEY = "zookeeper.sasl.clientconfig";
     public static final String ENABLE_CLIENT_SASL_KEY = "zookeeper.sasl.client";
-    public static final String ENABLE_CLIENT_SASL_DEFAULT = "true";
+
+    // Fabric8: We disable SASL by default
+    public static final String ENABLE_CLIENT_SASL_DEFAULT = "false";
     private static volatile boolean initializedLogin = false; 
 
     /**
@@ -62,7 +62,9 @@ public class ZooKeeperSaslClient {
      * @return If the SASL client is enabled.
      */
     public static boolean isEnabled() {
-        return Boolean.valueOf(System.getProperty(ENABLE_CLIENT_SASL_KEY, ENABLE_CLIENT_SASL_DEFAULT));
+        // Fabric8: We disable SASL by default
+        return false;
+//        return Boolean.valueOf(System.getProperty(ENABLE_CLIENT_SASL_KEY, ENABLE_CLIENT_SASL_DEFAULT));
     }
 
     private static final Logger LOG = LoggerFactory.getLogger(ZooKeeperSaslClient.class);
@@ -94,11 +96,18 @@ public class ZooKeeperSaslClient {
 
     public ZooKeeperSaslClient(final String serverPrincipal)
             throws LoginException {
-        /**
-         * ZOOKEEPER-1373: allow system property to specify the JAAS
-         * configuration section that the zookeeper client should use.
-         * Default to "Client".
-         */
+        // Fabric8: We disable SASL by default
+        this.saslState = SaslState.FAILED;
+        this.configStatus = "Will not attempt to authenticate using SASL (unknown error)";
+        this.isSASLConfigured = false;
+    }
+
+    // Fabric8: We disable SASL by default
+    /*public ZooKeeperSaslClient(final String serverPrincipal)
+            throws LoginException {
+        // ZOOKEEPER-1373: allow system property to specify the JAAS
+        // configuration section that the zookeeper client should use.
+        // Default to "Client".
         String clientSection = System.getProperty(ZooKeeperSaslClient.LOGIN_CONTEXT_NAME_KEY, "Client");
         // Note that 'Configuration' here refers to javax.security.auth.login.Configuration.
         AppConfigurationEntry entries[] = null;
@@ -162,7 +171,7 @@ public class ZooKeeperSaslClient {
                 }
             }
         }
-    }
+    }*/
     
     /**
      * @return informational message indicating the current configuration status.
