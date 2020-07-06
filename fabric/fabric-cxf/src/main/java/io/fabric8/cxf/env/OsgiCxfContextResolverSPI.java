89c2c774b3887f49c52db650fdd866d66f5664cc
Freeman Fang <freeman.fang@gmail.com>
2020-07-06 14:53:50 -0400

    [ENTESB-14245]Fabric Load Balancing for CXF publish a wrong URL on ZK

--- fabric/fabric-cxf/src/main/java/io/fabric8/cxf/env/OsgiCxfContextResolverSPI.java
+++ fabric/fabric-cxf/src/main/java/io/fabric8/cxf/env/OsgiCxfContextResolverSPI.java
@@ -15,38 +15,55 @@
  */
 package io.fabric8.cxf.env;
 
+import java.io.IOException;
+import java.util.Dictionary;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import javax.servlet.Servlet;
 import javax.servlet.http.HttpServlet;
 
+import org.apache.cxf.common.logging.LogUtils;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.FrameworkUtil;
 import org.osgi.framework.ServiceReference;
+import org.osgi.service.cm.Configuration;
+import org.osgi.service.cm.ConfigurationAdmin;
 import org.osgi.util.tracker.ServiceTracker;
+
+import io.fabric8.cxf.endpoint.ManagedApi;
 
 public class OsgiCxfContextResolverSPI extends CxfContextResolver.CxfContextResolverSPI {
 
-    private BundleContext systemContext;
+   
     private String cxfServletContext;
-    private ServiceTracker<Servlet, Servlet> tracker;
+ 
+    private ConfigurationAdmin configurationAdmin;
+    
+    private static final Logger LOG = LogUtils.getL7dLogger(OsgiCxfContextResolverSPI.class);
 
     public OsgiCxfContextResolverSPI() {
         Bundle bundle = FrameworkUtil.getBundle(OsgiCxfContextResolverSPI.class);
         if (bundle != null) {
-            systemContext = bundle.getBundleContext().getBundle(0).getBundleContext();
-            tracker = new ServiceTracker<Servlet, Servlet>(systemContext, Servlet.class, null);
-            tracker.open();
-            ServiceReference<Servlet>[] srs = tracker.getServiceReferences();
-            // cxf-rt-transports-http registers org.apache.cxf.transport.servlet.CXFNonSpringServlet in OSGi env
-            // as javax.servlet.Servlet service
-            for (ServiceReference<Servlet> sr : srs) {
-                Servlet s = systemContext.getService(sr);
-                if (s instanceof HttpServlet && s.getClass().getPackage().getName().startsWith("org.apache.cxf")) {
-                    this.cxfServletContext = (String) sr.getProperty("alias");
-                    break;
+            this.cxfServletContext = "/cxf";
+            if (getConfigurationAdmin() != null) {
+                try {
+                    Configuration configuration = getConfigurationAdmin().getConfiguration("org.apache.cxf.osgi", null);
+                    if (configuration != null) {
+                        Dictionary properties = configuration.getProperties();
+                        if (properties != null) {
+                            String servletContext = (String)configuration.getProperties().
+                                get("org.apache.cxf.servlet.context");
+                            if (servletContext != null) {
+                                this.cxfServletContext = servletContext;
+                            }
+                        }
+                    }
+                } catch (IOException e) {
+                    LOG.log(Level.WARNING, "getServletContext failed.", e);
                 }
             }
-            tracker.close();
         } else {
             throw new UnsupportedOperationException("OSGi Framework not detected");
         }
@@ -57,4 +74,20 @@ public class OsgiCxfContextResolverSPI extends CxfContextResolver.CxfContextReso
         return cxfServletContext;
     }
 
+    private ConfigurationAdmin getConfigurationAdmin() {
+       
+        if (configurationAdmin == null) {
+            BundleContext bundleContext = FrameworkUtil.getBundle(ManagedApi.class).getBundleContext();
+            if (bundleContext != null) {
+                ServiceReference serviceReference = bundleContext
+                    .getServiceReference(ConfigurationAdmin.class.getName());
+                if (serviceReference != null) {
+                    configurationAdmin = (ConfigurationAdmin)bundleContext.getService(serviceReference);
+                }
+            }
+
+        }
+
+        return configurationAdmin;
+    }
 }
